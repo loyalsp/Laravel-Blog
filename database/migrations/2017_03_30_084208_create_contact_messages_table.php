4b8e9d04861b964daacf275b6f6e1ecde7ede93f
loyalsp <adnann.rasheed@gmail.com>
2017-03-30 15:51:44 +0500

    #1 designed relational database and run the migrations

---
+++ database/migrations/2017_03_30_084208_create_contact_messages_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateContactMessagesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('contact_messages', function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestamps();
+            $table->string('sender');
+            $table->string('email');
+            $table->string('subject');
+            $table->string('body');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('contact_messages');
+    }
+}
---
+++ database/seeds/AdminTableSeeder.php
@@ -0,0 +1,20 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+
+class AdminTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $admin = new \App\Admin();
+        $admin->email = "admin@admin.com";
+        $admin->password = bcrypt("abc123");
+        $admin->save();
+    }
+}
