0bf9e42dde78c4ec530e7e9bb98a2a0adeea7859
loyalsp <adnann.rasheed@gmail.com>
2017-05-06 17:04:53 +0500

    send contact message and emails by using event and listeners

---
+++ app/Listeners/SendNotification.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Listeners;
+
+use App\Events\MessageSent;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Support\Facades\Mail;
+
+class SendNotification
+{
+    /**
+     * Create the event listener.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Handle the event.
+     *
+     * @param  MessageSent  $event
+     * @return void
+     */
+    public function handle(MessageSent $event)
+    {
+        $contact_message = $event->message;
+        Mail::send('email.contact-message-notification',['contact_message' => $contact_message],function($m) use($contact_message)
+        {
+            $m->from('info@adnan.com','Adnan Rasheed');
+            $m->to($contact_message->email, $contact_message->sender);
+            $m->subject('we have received your message');
+        });
+
+    }
+}
---
+++ resources/views/email/contact-message-confirmation.blade.php
@@ -0,0 +1,4 @@
+<h1>We received you message</h1>
+<p>Thank you, we will respond you as soon as possible</p>
+<h3>information about you message</h3>
+<p> Subject: {{$contact_message->subject}}</p>
